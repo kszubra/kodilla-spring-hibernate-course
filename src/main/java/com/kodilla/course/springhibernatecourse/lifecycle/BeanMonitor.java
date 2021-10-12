21c2fac8be64f339071e7ec5eb21f2f97ac6efc2
Kacper Szubra <kacper.szubra@nvt.pl>
2021-10-12 22:27:12 +0200

    Added comment

--- src/main/java/com/kodilla/course/springhibernatecourse/lifecycle/BeanMonitor.java
+++ src/main/java/com/kodilla/course/springhibernatecourse/lifecycle/BeanMonitor.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 
 public class BeanMonitor implements BeanPostProcessor {
 
+    //Metody te pozwalają na zmodyfikowanie beana w trakcie jego tworzenia – odpowiednio przed i po inicjalizacji.
+    // Można ten mechanizm wykorzystać do globalnej zmiany zachowania niektórych beanów – pisząc kod tylko w tym jednym miejscu.
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws
             BeansException {
