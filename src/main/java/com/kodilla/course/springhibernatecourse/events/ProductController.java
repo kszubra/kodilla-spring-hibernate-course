879820949b2f20b5b13099f21271406d80f824a9
Kacper Szubra <kacper.szubra@nvt.pl>
2021-10-13 18:48:24 +0200

    Events

---
+++ src/main/java/com/kodilla/course/springhibernatecourse/events/ProductController.java
@@ -0,0 +1,31 @@
+package com.kodilla.course.springhibernatecourse.events;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/products")
+public class ProductController implements ApplicationEventPublisherAware {
+    private ApplicationEventPublisher publisher;
+
+    @PostMapping(path = "createProduct")
+    public void createProduct() {
+        System.out.println("Register product:");
+
+        publisher.publishEvent(
+                new ProductRegisteredEvent(
+                        this,
+                        "AAA",
+                        "BBB"));
+    }
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.publisher = applicationEventPublisher;
+    }
+
+}
