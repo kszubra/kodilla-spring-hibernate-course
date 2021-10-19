b7aaa0484613f315ada8377d57d7893a8595d189
Kacper Szubra <kacper.szubra@nvt.pl>
2021-10-19 18:49:44 +0200

    proxy example

---
+++ src/main/java/com/kodilla/course/springhibernatecourse/proxy/PostgresDataRetriever.java
@@ -0,0 +1,24 @@
+package com.kodilla.course.springhibernatecourse.proxy;
+
+public class PostgresDataRetriever implements DbDataRetriever {
+
+    public PostgresDataRetriever() throws InterruptedException {
+        Thread.sleep(5000);
+    }
+
+    @Override
+    public int getFirstValue() {
+        return 100;
+    }
+
+    @Override
+    public int getSecondValue() {
+        return 200;
+    }
+
+    @Override
+    public int getThirdValue() {
+        return 300;
+    }
+
+}
